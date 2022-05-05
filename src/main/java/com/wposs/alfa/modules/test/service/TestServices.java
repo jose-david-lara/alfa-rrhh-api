302c9e2231604afc45799d956356dfda086212b9
Jose Lara <joselara@wposs.com>
2022-05-05 16:31:34 -0400

    WP-R001-CONSTRUCCION-ARQUITECTURA
    * Nueva implementacion de arquitectura con Firmware exclusivo

--- src/main/java/com/wposs/alfa/modules/test/service/TestServices.java
+++ src/main/java/com/wposs/alfa/modules/test/service/TestServices.java
@@ -5,19 +5,23 @@ import java.util.Map;
 import org.springframework.stereotype.Component;
 
 import com.wposs.alfa.modules.test.repository.TestRepository;
-import com.wposs.core.repository.Transaction;
-import com.wposs.core.service.BaseSpringService;
+import com.wposs.alfa_framework.spring.ResponseModel;
 
 @Component
-public class TestServices extends BaseSpringService<TestRepository>{
+public class TestServices extends TestRepository{
 	
-	public Map<String, Object> getTEST(Map<String, Object> request) throws Exception {
-		return beginReadTransaction(new Transaction<Map<String, Object>>() {
-			public Map<String, Object> doTransaction() throws Exception{
-				return getRepository().getTEST(this, request);
-			}
-			
-		});
+	private ResponseModel rspModel;
+	
+	public ResponseModel getTEST(Map<String, Object> request) throws Exception {
+		
+		 rspModel = new ResponseModel();
+		 
+		 rspModel.setCode("0");
+		 rspModel.setMessage("exitoso");
+		 rspModel.setError(false);
+		 rspModel.setData(getTESTRepository(request));
+		
+		return rspModel;
 				
 	}
 }
