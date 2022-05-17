5199da877ee65e66a3d976de0bfa2f5278652089
Jose Lara <joselara@wposs.com>
2022-05-17 16:21:14 -0400

    DEVELOP
    * Correccion de test para guia programacion

--- src/main/java/com/wposs/alfa/modules/test/service/TestServices.java
+++ src/main/java/com/wposs/alfa/modules/test/service/TestServices.java
@@ -4,6 +4,7 @@ import java.util.Map;
 
 import org.springframework.stereotype.Component;
 
+import com.wposs.alfa.modules.test.dto.TestInput;
 import com.wposs.alfa.modules.test.repository.TestRepository;
 import com.wposs.alfa_framework.spring.ResponseModel;
 
@@ -12,14 +13,14 @@ public class TestServices extends TestRepository{
 	
 	private ResponseModel rspModel;
 	
-	public ResponseModel getTEST(Map<String, Object> request) throws Exception {
+	public ResponseModel getTESTServices(TestInput inputTest) throws Exception {
 		
 		 rspModel = new ResponseModel();
 		 
 		 rspModel.setCode("0");
 		 rspModel.setMessage("exitoso");
 		 rspModel.setError(false);
-		 rspModel.setData(getTESTRepository(request));
+		 rspModel.setData(getTESTRepository(inputTest));
 		
 		return rspModel;
 				
